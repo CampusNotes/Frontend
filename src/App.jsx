6800ec1cd05acfe336730a0233394d9314cb6710
unknown <prathameshshriramwar100@gmail.com>
2024-02-25 17:09:12 +0530

    prs

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-<<<<<<< HEAD
-import { Button } from "@material-tailwind/react";
-import Home from "./pages/Home";
-=======
 import { Route, Routes } from "react-router"
 import Home from "./pages/Home"
 
->>>>>>> cf0e9805dac25a3e10c0bbb18afbf3758c2bcfd0
 
 function App() {
 
 
   return (
-<<<<<<< HEAD
-    <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-      <Button>Button</Button>;
-
-      <Home/>
-    </>
-=======
     <Routes>
       <Route path='/' element={<Home />} />
     </Routes>
->>>>>>> cf0e9805dac25a3e10c0bbb18afbf3758c2bcfd0
   )
 }
 
