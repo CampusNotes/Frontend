fbadb1ceb5f014c446392d04dad4a817cd426ee5
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-02-25 17:12:19 +0530

    Updated Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Navbarcomponent from "../components/Navbarcomponent"
+import Footer from "../components/Footer"
+
+
+function Home() {
+  return (
+    <div>
+      <Navbarcomponent />
+
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
