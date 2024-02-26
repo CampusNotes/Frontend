aae7c8133997f7242418545071f01424deb28606
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-02-26 12:23:59 +0530

    animated blob added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ export default withMT({
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob_animate: 'blob 8s ease-in-out infinite'
+      },
+      keyframes: {
+        blob: {
+          '0%': { transform: 'scale(1) translateX(0%) translateY(0%)' },
+          '25%': { transform: 'scale(1.3) translateX(20%) translateY(20%)' },
+          '50%': { transform: 'scale(1.9) translateX(-20%) translateY(-20%)' },
+          '75%': { transform: 'scale(1.3) translateX(10%) translateY(10%)' },
+          '100%': { transform: 'scale(1) translateX(0%) translateY(0%)' }
+        }
+      }
+    },
   },
   plugins: [],
 })
